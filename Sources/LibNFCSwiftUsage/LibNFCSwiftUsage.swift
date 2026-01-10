81dbffc7de7067d4c3c605982d638f3f48d7ce5f
Diggory Laycock <nospam@diggory.net>
2026-01-10 18:03:54 +0000

    Changed from local package dep to github version.

--- Sources/LibNFCSwiftUsage/LibNFCSwiftUsage.swift
+++ Sources/LibNFCSwiftUsage/LibNFCSwiftUsage.swift
@@ -1,14 +1,16 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-// 
-// Swift Argument Parser
-// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
-
 import ArgumentParser
+import LibNFCSwift
 
 @main
 struct LibNFCSwiftUsage: ParsableCommand {
     mutating func run() throws {
-        print("Hello, world!")
+        print("Hello, world.  Testing LibNFCSwift!")
+
+        let nfc = NFC()
+        print ("LibNFC version \(nfc.libNFCVersion() ?? "Version not available")")
+
+        nfc.nfcInit()
+        nfc.nfcOpen()
+        print("NFC Device name: \(nfc.deviceName() ?? "Unknown device name")")
     }
 }
